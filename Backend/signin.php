9ddfb248c9a72dcb797536790feaad6fdc1624e9
ToniTannoury <tatannoury01@gmail.com>
2023-07-20 02:37:41 +0300

    Checking request body , email and password

--- Backend/signin.php
+++ Backend/signin.php
@@ -4,3 +4,15 @@ include("connection.php");
 
 $response = array();
 $json_data = file_get_contents('php://input');
+
+if ($json_data) {
+  $data = json_decode($json_data, true);
+  if (isset($data['email']) && isset($data['password'])) {
+    $email = $data['email'];
+    $password = $data['password'];
+  } else {
+    $response = array('status' => 'email and password not provided');
+  }
+} else {
+  $response = array('status' => 'failed', 'error' => 'Invalid request body');
+}
