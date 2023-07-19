c390d7a922ee58df1c8cd0b93b52089d1f4627f2
ToniTannoury <tatannoury01@gmail.com>
2023-07-20 02:50:23 +0300

    checking if the email , phone number and usernamer are unique

--- Backend/signup.php
+++ Backend/signup.php
@@ -21,6 +21,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $data['password'];
     $email = $data['email'];
     $phone = $data['phone'];
+
+    $check_username = $mysqli->prepare("SELECT username FROM users WHERE username = ?");
+    $check_username->bind_param('s', $username);
+    $check_username->execute();
+    $check_username->store_result();
+    $username_exists = $check_username->num_rows();
+
+    $check_email = $mysqli->prepare("SELECT email FROM users WHERE email = ?");
+    $check_email->bind_param('s', $email);
+    $check_email->execute();
+    $check_email->store_result();
+    $email_exists = $check_email->num_rows();
+
+    $check_phone = $mysqli->prepare("SELECT phone FROM users WHERE phone = ?");
+    $check_phone->bind_param('s', $phone);
+    $check_phone->execute();
+    $check_phone->store_result();
+    $phone_exists = $check_phone->num_rows();
   }else {
       $response['status'] = 'failed';
       $response['error'] = 'Missing required fields.';
