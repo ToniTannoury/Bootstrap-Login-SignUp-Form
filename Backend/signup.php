7c47b6aee65263ffc3fb8fb6cde13f41b2b8d72a
ToniTannoury <tatannoury01@gmail.com>
2023-07-20 02:48:22 +0300

    checking for all input fields and assigning them to variables

--- Backend/signup.php
+++ Backend/signup.php
@@ -11,6 +11,21 @@ $response = array();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $json_data = file_get_contents('php://input');
   $data = json_decode($json_data, true);
+  if (
+    isset($data['username']) &&
+    isset($data['password']) &&
+    isset($data['email']) &&
+    isset($data['phone'])
+    ) {
+    $username = $data['username'];
+    $password = $data['password'];
+    $email = $data['email'];
+    $phone = $data['phone'];
+  }else {
+      $response['status'] = 'failed';
+      $response['error'] = 'Missing required fields.';
+    }
+
 }else {
   $response['status'] = 'failed';
   $response['error'] = 'Invalid request method.';
