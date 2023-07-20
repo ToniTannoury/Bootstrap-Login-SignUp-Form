5faf17d8b3aed31dbd4da7f9a4cc049ed0416d6f
ToniTannoury <tatannoury01@gmail.com>
2023-07-20 12:18:44 +0300

    Validating user input

--- Backend/signup.php
+++ Backend/signup.php
@@ -22,39 +22,57 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $data['email'];
     $phone = $data['phone'];
 
-    $check_username = $mysqli->prepare("SELECT username FROM users WHERE username = ?");
-    $check_username->bind_param('s', $username);
-    $check_username->execute();
-    $check_username->store_result();
-    $username_exists = $check_username->num_rows();
-
-    $check_email = $mysqli->prepare("SELECT email FROM users WHERE email = ?");
-    $check_email->bind_param('s', $email);
-    $check_email->execute();
-    $check_email->store_result();
-    $email_exists = $check_email->num_rows();
-
-    $check_phone = $mysqli->prepare("SELECT phone FROM users WHERE phone = ?");
-    $check_phone->bind_param('s', $phone);
-    $check_phone->execute();
-    $check_phone->store_result();
-    $phone_exists = $check_phone->num_rows();
-
-    if ($username_exists === 0 && $email_exists === 0 && $phone_exists === 0) {
-      $hashed_password = password_hash($password, PASSWORD_BCRYPT);
-
-      $query = $mysqli->prepare('INSERT INTO users (username, password, phone, email) VALUES (?, ?, ?, ?)');
-      $query->bind_param('ssss', $username, $hashed_password, $phone, $email);
-      if ($query->execute()) {
-        $response['status'] = 'success';
-        $response['data'] = ["username"=>$username , "phone"=>$phone, 'email'=>$email];
-      } else {
-        $response['status'] = 'failed';
-        $response['error'] = 'Error executing query: ' . $mysqli->error;
-      }
-    } else {
+    // Validate username length (at least 6 characters)
+    if (strlen($username) < 6) {
       $response['status'] = 'failed';
-      $response['error'] = 'Username, email, or phone already exists.';
+      $response['error'] = 'Username should be at least 6 characters long.';
+    } else {
+      // Validate password length (at least 6 characters)
+      if (strlen($password) < 6) {
+        $response['status'] = 'failed';
+        $response['error'] = 'Password should be at least 6 characters long.';
+      } else {
+        // Validate email format
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+          $response['status'] = 'failed';
+          $response['error'] = 'Invalid email format.';
+        } else {
+          $check_username = $mysqli->prepare("SELECT username FROM users WHERE username = ?");
+          $check_username->bind_param('s', $username);
+          $check_username->execute();
+          $check_username->store_result();
+          $username_exists = $check_username->num_rows();
+
+          $check_email = $mysqli->prepare("SELECT email FROM users WHERE email = ?");
+          $check_email->bind_param('s', $email);
+          $check_email->execute();
+          $check_email->store_result();
+          $email_exists = $check_email->num_rows();
+
+          $check_phone = $mysqli->prepare("SELECT phone FROM users WHERE phone = ?");
+          $check_phone->bind_param('s', $phone);
+          $check_phone->execute();
+          $check_phone->store_result();
+          $phone_exists = $check_phone->num_rows();
+
+          if ($username_exists === 0 && $email_exists === 0 && $phone_exists === 0) {
+            $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+
+            $query = $mysqli->prepare('INSERT INTO users (username, password, phone, email) VALUES (?, ?, ?, ?)');
+            $query->bind_param('ssss', $username, $hashed_password, $phone, $email);
+            if ($query->execute()) {
+              $response['status'] = 'success';
+              $response['data'] = ["username" => $username, "phone" => $phone, 'email' => $email];
+            } else {
+              $response['status'] = 'failed';
+              $response['error'] = 'Error executing query: ' . $mysqli->error;
+            }
+          } else {
+            $response['status'] = 'failed';
+            $response['error'] = 'Username, email, or phone already exists.';
+          }
+        }
+      }
     }
   } else {
     $response['status'] = 'failed';
