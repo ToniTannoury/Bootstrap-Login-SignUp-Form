a4b38aa0f77b94ff19b0970367a42a8e8c387eea
ToniTannoury <tatannoury01@gmail.com>
2023-07-20 03:21:54 +0300

    Fixing confirm password

--- signup.js
+++ signup.js
@@ -8,6 +8,7 @@ const regEmail = document.querySelector(".regEmail").value.trim();
 const regPhone = document.querySelector(".regPhone").value.trim();
 const regConfirm = document.querySelector(".regConfirm").value.trim();
 const regPass = document.querySelector(".regPass").value.trim();
+
 if(regConfirm!==regPass){
   document.getElementById("errorDiv").classList.remove('d-none');
   document.getElementById("errorDiv").textContent = "Passwords do not match";
@@ -17,23 +18,25 @@ if(regConfirm!==regPass){
   return;
 }
 if (!regUserName || !regEmail || !regPhone || !regConfirm || !regPass) {
-  console.error();
+    console.error();
 
-  document.getElementById("errorDiv").classList.remove('d-none');
-  document.getElementById("errorDiv").textContent = "Please fill in all required fields.";
-  setTimeout(()=>{
-    document.getElementById("errorDiv").classList.add('d-none');
-  },3000)
-  return;
-}
+    document.getElementById("errorDiv").classList.remove('d-none');
+    document.getElementById("errorDiv").textContent = "Please fill in all required fields.";
+    setTimeout(()=>{
+      document.getElementById("errorDiv").classList.add('d-none');
+    },3000)
+    return;
+  }
 
-const signUpInput = {
-  username: regUserName,
-  password: regPass,
-  phone: regPhone,
-  email: regEmail,
-};
-const url = 'http://localhost/SignUpLogin/Backend/signup.php';
+  const signUpInput = {
+    username: regUserName,
+    password: regPass,
+    phone: regPhone,
+    email: regEmail,
+  };
+
+  console.log(signUpInput);
+  const url = 'http://localhost/SignUpLogin/signup.php';
   const body = JSON.stringify(signUpInput);
   console.log(body)
   fetch(url, {
@@ -53,8 +56,12 @@ const url = 'http://localhost/SignUpLogin/Backend/signup.php';
         document.getElementById("errorDiv").classList.add('d-none');
       },3000)
     }
+    localStorage.setItem('responseData', JSON.stringify(data));
+    window.location.href = 'dashboard.html';
   })
   .catch(error => {
+    
     console.log(error);
   }); 
-}
+};
+
