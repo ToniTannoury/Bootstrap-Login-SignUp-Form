ce4295022c44b464ecba45c54569f7bd75ab5096
ToniTannoury <tatannoury01@gmail.com>
2023-07-20 03:07:45 +0300

    Adjusting sign up file

--- signup.js
+++ signup.js
@@ -1,14 +1,12 @@
-window.onload = function() {
-  console.log(11);
+const signUp = document.querySelector(".signUp").addEventListener("click", (e) => {
+  e.preventDefault();
+  postSignUpInput();
+});
+const postSignUpInput = () => {
+  const regUserName = document.querySelector(".regUserName").value.trim();
+const regEmail = document.querySelector(".regEmail").value.trim();
+const regPhone = document.querySelector(".regPhone").value.trim();
+const regConfirm = document.querySelector(".regConfirm").value.trim();
+const regPass = document.querySelector(".regPass").value.trim();
 
-  const signIn = document.querySelector(".signIn").addEventListener("click", (e) => {
-    e.preventDefault();
-    postLoginInput();
-  });
-
-  const postLoginInput = () => {
-    const email = document.querySelector(".email").value;
-    const password = document.querySelector(".password").value;
-
-  }
 }
