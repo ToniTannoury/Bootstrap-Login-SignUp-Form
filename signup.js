c5c416a3fd734435b47457533ab55f54f632caf3
ToniTannoury <tatannoury01@gmail.com>
2023-07-20 03:08:45 +0300

    Performing client-side validation

--- signup.js
+++ signup.js
@@ -8,5 +8,15 @@ const regEmail = document.querySelector(".regEmail").value.trim();
 const regPhone = document.querySelector(".regPhone").value.trim();
 const regConfirm = document.querySelector(".regConfirm").value.trim();
 const regPass = document.querySelector(".regPass").value.trim();
+if (!regUserName || !regEmail || !regPhone || !regConfirm || !regPass) {
+  console.error();
+
+  document.getElementById("errorDiv").classList.remove('d-none');
+  document.getElementById("errorDiv").textContent = "Please fill in all required fields.";
+  setTimeout(()=>{
+    document.getElementById("errorDiv").classList.add('d-none');
+  },3000)
+  return;
+}
 
 }
